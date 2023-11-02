4a5d0a49eb1b06322475a4dacd529116bd7d1781
Brian Lee <brianlee@Brians-MacBook-Pro-2.local>
2023-11-02 11:24:43 -0700

    Finished CategoryBubbles

---
+++ client/src/utils/queries.js
@@ -0,0 +1,26 @@
+import { gql } from '@apollo/client';
+
+export const QUERY_PRODUCTS = gql`
+  query getProducts($category: ID) {
+    products(category: $category) {
+      _id
+      name
+      description
+      price
+      quantity
+      image
+      category {
+        _id
+      }
+    }
+  }
+`;
+
+export const QUERY_CATEGORIES = gql`
+  {
+    categories {
+      _id
+      name
+    }
+  }
+`;
