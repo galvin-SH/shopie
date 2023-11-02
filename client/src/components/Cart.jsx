a47857a026720bc7955075b1d9be7531ca9a296b
Drinkmilk <141216114+lndvng@users.noreply.github.com>
2023-11-02 13:20:34 -0700

    updated btn text

--- client/src/components/Cart.jsx
+++ client/src/components/Cart.jsx
@@ -7,14 +7,13 @@ function Cart() {
 
   return (
     <>
-      <Button onClick={() => setOpenModal(true)}>Toggle modal</Button>
+      <Button onClick={() => setOpenModal(true)}>CART</Button>
       <Modal show={openModal} onClose={() => setOpenModal(false)}>
-        <Modal.Header>Terms of Service</Modal.Header>
+        <Modal.Header>YOUR CART</Modal.Header>
         <Modal.Body>
           <div className="space-y-6">
             <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-              With less than a month to go before the European Union enacts new consumer privacy laws for its citizens,
-              companies around the world are updating their terms of service agreements to comply.
+              Item image preview
             </p>
             <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
               The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25 and is meant
@@ -24,9 +23,9 @@ function Cart() {
           </div>
         </Modal.Body>
         <Modal.Footer>
-          <Button onClick={() => setOpenModal(false)}>I accept</Button>
+          <Button onClick={() => setOpenModal(false)}>CHECKOUT</Button>
           <Button color="gray" onClick={() => setOpenModal(false)}>
-            Decline
+            CLOSE
           </Button>
         </Modal.Footer>
       </Modal>
