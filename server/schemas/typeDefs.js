b30fd382361d28cdb405f6de97e7e61b71a4db76
Cossack <kkowalak9@gmail.com>
2023-11-01 23:04:11 -0700

    Finished basic schemas

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,27 @@
+const typeDefs = `
+  type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    getSingleUser: User
+  }
+
+
+  type Mutation {
+    createUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+  }
+`;
+
+module.exports = typeDefs;
