840a860af7b7ac7b666ca4e9a8942ab9b071b3dd
Cossack <kkowalak9@gmail.com>
2023-11-02 22:55:49 -0700

    Saving changes

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,29 +1,50 @@
+import { useState } from 'react';
 import { Link } from "react-router-dom";
-import { Navbar } from "flowbite-react";
+import { Navbar, Modal, Tabs } from "flowbite-react";
 import LogoWhite from "../assets/images/HAPPY-SNACKS-white.gif"
 import Cart from "../components/Cart";
+import SignUpForm from './SignupForm';
+import LoginForm from './LoginForm';
+import Auth from '../utils/auth';
+
+
 
 function Nav() {
-  return (
-    <Navbar fluid rounded className="border-2 rounded-lg p-3 border-gray-300">
-      <Navbar.Brand as={Link} href="https://flowbite-react.com">
-        <img src={LogoWhite} className="mr-3 h-6 sm:h-9" alt="Happy Snack" />
-      </Navbar.Brand>
-      <Navbar.Toggle />
-      <Navbar.Collapse>
-        <Navbar.Link as={Link} to="#" active>
-          Home
-        </Navbar.Link>
-        <Navbar.Link as={Link} to="#">
-          About
-        </Navbar.Link>
-        <Navbar.Link as={Link} to="#">Menu</Navbar.Link>
-        <Navbar.Link as={Link} to="#">Login</Navbar.Link>
-        <Navbar.Link as={Link} to="#">Dark Mode</Navbar.Link>
-        <Cart/>
-      </Navbar.Collapse>
-    </Navbar>
-  );
+	const [showModal, setShowModal] = useState(false);
+
+	return (
+		<>
+			<Navbar fluid rounded className="border-2 rounded-lg p-3 border-gray-300">
+				<Navbar.Brand as={Link} href="https://flowbite-react.com">
+					<img src={LogoWhite} className="mr-3 h-6 sm:h-9" alt="Happy Snack" />
+				</Navbar.Brand>
+				<Navbar.Toggle />
+				<Navbar.Collapse>
+					<Navbar.Link as={Link} to="#" active>Home</Navbar.Link>
+					<Navbar.Link as={Link} to="#">About</Navbar.Link>
+					<Navbar.Link as={Link} to="#">Menu</Navbar.Link>
+					{Auth.loggedIn() ? (
+						<>
+							<Navbar.Link onClick={Auth.logout}>Logout</Navbar.Link>
+						</>
+					) : (
+						<Navbar.Link onClick={() => setShowModal(true)}>Login/Sign Up</Navbar.Link>
+
+					)}
+					<Navbar.Link as={Link} to="#">Dark Mode</Navbar.Link>
+					<Cart />
+				</Navbar.Collapse>
+			</Navbar>
+			<Modal dismissible show={showModal} onClose={() => setShowModal(false)}>
+                <Modal.Header>
+                    Login/Sign Up
+                </Modal.Header>
+                <Modal.Body>
+
+                </Modal.Body>
+            </Modal>
+		</>
+	);
 }
 
 export default Nav;
---
+++ client/src/utils/mutations.js
@@ -0,0 +1,36 @@
+import { gql } from '@apollo/client';
+
+export const CREATE_USER = gql`
+  mutation createUser($username: String!, $email: String!, $password: String!) {
+    createUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+        password
+        bookCount
+        savedBooks {
+          bookId
+          authors
+          description
+          image
+          link
+          title
+        }
+      }
+    }
+  }
+`;
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
