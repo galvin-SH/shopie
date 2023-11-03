2355a48bf8eeded94ab40fd2d7aa2e25c6d0513e
Cossack <kkowalak9@gmail.com>
2023-11-03 10:59:35 -0700

    Saving work

--- client/src/components/CategoryFilter.jsx
+++ client/src/components/CategoryFilter.jsx
@@ -1,44 +1,52 @@
 import React from 'react';
+import { useState } from 'react';
 import { useQuery } from '@apollo/client';
 import { QUERY_CATEGORIES } from '../utils/queries';
 import { useStoreContext } from '../utils/GlobalState';
 
+const style = {
+  borderStyle: "solid",
+  borderWidth: "5px",
+  float: "left",
+  width: "400px"
+};
+
 // Still needs work done. Will come back to it
 
 function CategoryFilter() {
-  const { loading, error, data } = useQuery(QUERY_CATEGORIES);
-  const [selectedCategories, setSelectedCategories] = useState([]);
+  //const { loading, error, data } = useQuery(QUERY_CATEGORIES);
+  //const [selectedCategories, setSelectedCategories] = useState([]);
 
-  const [state, dispatch] = useStoreContext();
+  //const [state, dispatch] = useStoreContext();
 
-  const onClick = (category) => {
-    if (selectedCategories.includes(category)) {
-      const updatedCategories = setSelectedCategories.filter((category) => category !== DeselectCategory);
+  // const onClick = (category) => {
+  //   if (selectedCategories.includes(category)) {
+  //     const updatedCategories = setSelectedCategories.filter((category) => category !== DeselectCategory);
 
-      setSelectedCategories(updatedCategories);
+  //     setSelectedCategories(updatedCategories);
 
-      dispatch({
-        type: "UPDATE_CATEGORIES",
-        categories: updatedCategories,
-      });
-    } else {
-      setSelectedCategories([...selectedCategories, category]);
+  //     dispatch({
+  //       type: "UPDATE_CATEGORIES",
+  //       categories: updatedCategories,
+  //     });
+  //   } else {
+  //     setSelectedCategories([...selectedCategories, category]);
 
-      dispatch({
-        type: "UPDATE_CATEGORIES",
-        categories: [...selectedCategories, category],
-      })
+  //     dispatch({
+  //       type: "UPDATE_CATEGORIES",
+  //       categories: [...selectedCategories, category],
+  //     })
 
-    }
-  }
+  //   }
+  // }
 
-  const categories = data.categories;
+  //const categories = data.categories;
 
   return (
-    <div>
+    <div style={style}>
       <h2>Categories</h2>
       <div>
-        {categories.map((category) => (
+        {/* {categories.map((category) => (
           <label key={category._id}>
             <input
               type="checkbox"
@@ -47,8 +55,10 @@ function CategoryFilter() {
             />
             {category.name}
           </label>
-        ))}
+        ))} */}
       </div>
     </div>
   )
 }
+
+export default CategoryFilter;
