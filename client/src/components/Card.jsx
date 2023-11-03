e21c7ce845ab6249edd6b3deac87f5e8b8671d32
alliedeaver <adeav97@gmail.com>
2023-11-03 00:28:12 -0700

    changes to cards in marquee, sizing and data

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,20 +1,38 @@
 import React from 'react';
+import Products2 from './Products2';
 
-const styles = {
-    card: {
-       width: "150px"
+// const styles = {
+//     card: {
+//        width: "50px"
 
-    }
-}
+//     }
+// }
+
+// const Products= ({data}) => {
+//     return (
+//         <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl group hover:bg-white-[50%] cursor-pointer'>
+//             {/* project image - hover effects and styling */}
+//             <img src={data.img} alt="/" className='rounded-xl group-hover:opacity-10' />
+//             <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
+//                 <h3 className='text-2xl text-white tracking-wider text-center'>
+//                     {data.title}
+//                     <p className='pb-4 pt-2 text-black text-center font-serif'>add to cart</p>
+//                 </h3>
+//             </div>
+//         </div>
+//     )
+// }
+
+// export default Products;
 
 function Card({data}) {
     console.log(data)
   return (
-    <div className="border-zinc-400 border-1 rounded-xl shadow-gray-400 shadow-xl flex-col h-25 w-50 hover:bg-white-[50%] cursor-pointer">
+    <div className="border-zinc-400 border-1 rounded-xl shadow-gray-400 shadow-xl flex-col h-150 w-60 hover:bg-white-[50%] cursor-pointer">
         <div >
-        <img  src={data.img}/>
+        <img   className="rounded-xl" src={data.img} style={{ width: '100%', height: 'auto' }}/>
         </div>
-        <h3 className="font-">
+        <h3 className="font-bold">
         {data.title}
         </h3>
         <p>
@@ -23,5 +41,7 @@ function Card({data}) {
     </div>
   )
 }
+
+//change writing to be appear when it hovers 
 
 export default Card
