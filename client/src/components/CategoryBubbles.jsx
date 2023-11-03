1243ca39d00202a16e845af50c7387f10958235e
alliedeaver <adeav97@gmail.com>
2023-11-03 16:39:05 -0700

    added style and fixed margin for the category buttons

--- client/src/components/CategoryBubbles.jsx
+++ client/src/components/CategoryBubbles.jsx
@@ -6,41 +6,59 @@ import Chocolate from "../assets/images/bubbles/Chocolate.png";
 import Drinks from "../assets/images/bubbles/Drinks.png";
 import Crackers from "../assets/images/bubbles/Cracker.png";
 
-import "../index.css";
 
 
 function CategoryBubbles() {
   return (
     <div className="bubble-container">
       <a href="#" target="_blank">
-        <div className="bubble">
-          <img src={Candy} alt="Candy" className="w-12" />
+        <div className="bubble hover:scale-110 ease-in duration-300 cursor-pointer">
+          <img src={Candy} alt="Candy"
+            className="w-12" />
         </div>
+        <p>
+          CANDY
+        </p>
       </a>
       <a href="#" target="_blank">
-        <div className="bubble">
+        <div className="bubble hover:scale-110 ease-in duration-300 cursor-pointer">
           <img src={Chips} alt="Chips" className="w-12" />
         </div>
+        <p>
+          CHIPS
+        </p>
       </a>
       <a href="#" target="_blank">
-        <div className="bubble">
+        <div className="bubble hover:scale-110 ease-in duration-300 cursor-pointer">
           <img src={Chocolate} alt="Chocolate" className="w-12" />
         </div>
+        <p>
+          CHOCOLATE
+        </p>
       </a>
       <a href="#" target="_blank">
-        <div className="bubble">
+        <div className="bubble hover:scale-110 ease-in duration-300 cursor-pointer">
           <img src={Crackers} alt="Crackers" className="w-12" />
         </div>
+        <p>
+          CRACKERS
+        </p>
       </a>
       <a href="#" target="_blank">
-        <div className="bubble">
+        <div className="bubble hover:scale-110 ease-in duration-300 cursor-pointer">
           <img src={Drinks} alt="Drinks" className="w-12" />
         </div>
+        <p>
+          DRINKS
+        </p>
       </a>
       <a href="#" target="_blank">
-        <div className="bubble">
+        <div className="bubble hover:scale-110 ease-in duration-300 cursor-pointer">
           <img src={Savory} alt="Savory" className="w-12" />
         </div>
+        <p>
+          SAVORY
+        </p>
       </a>
     </div>
   );
