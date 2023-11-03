21af1be9e3e84ecb004d97d5d0755a2c8447e1c4
alliedeaver <adeav97@gmail.com>
2023-11-03 11:52:19 -0700

    fixed sizing on marquee

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -4,19 +4,21 @@ import React from 'react';
 function Card({ data }) {
     console.log(data)
     return (
-        <div className="border-zinc-400 border-1 rounded-xl shadow-gray-400 shadow-xl flex-col h-150 w-60 group   cursor-pointer relative ">
+        <section className= "w-100% relative ">
+        <div className="border-zinc-400 border-1 rounded-xl shadow-gray-400 shadow-xl flex-col h-50 w-20 group   cursor-pointer relative ">
             <div >
                 <img className="rounded-xl " src={data.img} style={{ width: '100%', height: 'auto' }} />
             </div>
-            <div className="hidden group-hover:block absolute top-[25%] left-[50%] hover:bg-white-[50%] translate-x-[-50%] translate-y-[50%]" >
+            <div className="hidden group-hover:block absolute top-[10%] left-[50%] hover:bg-white-[50%] translate-x-[-50%] translate-y-[50%]" >
                 <h3>
                     {data.title}
                 </h3>
-                <p className="" >
+                <p className="font-size" >
                     {data.description}
                 </p>
             </div>
         </div>
+        </section>
     )
 }
 
