46399eae2d1ddf81ab2ee08b6e640d15f256b444
alliedeaver <adeav97@gmail.com>
2023-11-03 11:15:28 -0700

    changed the hover to go over the images

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,21 +1,25 @@
 import React from 'react';
 
 
-function Card({data}) {
+function Card({ data }) {
     console.log(data)
-  return (
-    <div className="border-zinc-400 border-1 rounded-xl shadow-gray-400 shadow-xl flex-col h-150 w-60 group hover:bg-white-[50%] cursor-pointer ">
-        <div >
-        <img className="rounded-xl " src={data.img} style={{ width: '100%', height: 'auto' }}/>
+    return (
+        <div className="border-zinc-400 border-1 rounded-xl shadow-gray-400 shadow-xl flex-col h-150 w-60 group   cursor-pointer relative ">
+            <div >
+                <img className="rounded-xl " src={data.img} style={{ width: '100%', height: 'auto' }} />
+            </div>
+            <div className="hidden group-hover:block absolute top-[25%] left-[50%] hover:bg-white-[50%] translate-x-[-50%] translate-y-[50%]" >
+                <h3>
+                    {data.title}
+                </h3>
+                <p className="" >
+                    {data.description}
+                </p>
+            </div>
         </div>
-        
-    </div>
-  )
+    )
 }
 
 //change writing to be appear when it hovers 
 
 export default Card
-
-
-// className="hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[50%]"
