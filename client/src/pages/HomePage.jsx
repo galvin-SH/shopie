74a13633249912f821911516a6f6bbd6e31737ca
alliedeaver <adeav97@gmail.com>
2023-11-03 18:19:20 -0700

    added to marquee

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,11 +1,11 @@
 import Marquee from "../components/Marquee"
 import ProductPage from "../pages/ProductPage";
-import CategoryFilter from "../components/CategoryFilter";
+// import CategoryFilter from "../components/CategoryFilter";
 
 const HomePage = () => {
     return (
         <div>
-            <h1>Home Page</h1>
+            
             <Marquee />
             <ProductPage />
             
