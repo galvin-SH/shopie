f1b9289e6d66cb4c03f92cb5d7443a05f83283b2
Cossack <kkowalak9@gmail.com>
2023-11-05 13:24:49 -0800

    Got products to render all items from global state and conditionally render the add to cart button based on login status

--- client/src/components/ProductSection.jsx
+++ client/src/components/ProductSection.jsx
@@ -1,11 +1,23 @@
+import { useGlobalState } from "../utils/GlobalState";
 import Product from "../components/Product";
 
 const ProductPage = () => {
+    const [state, dispatch] = useGlobalState();
+    console.log(state);
+
     return (
-        <div  >
-            
-            <div className="content-box absolute float-right"> 
-            <Product />
+        <div  >   
+            <div className="content-box absolute float-right">
+                {state.products.map((product) => (
+                <Product
+                    key={product._id}
+                    _id={product._id}
+                    imagePath={product.imagePath}
+                    productName={product.productName}
+                    productPrice={product.productPrice}
+                    categoryName={product.productCategory.categoryName}
+                />
+            ))} 
             </div>
         </div>
     );
