4e78afb728df9ad91bdca739632dc42d8c893bbd
alliedeaver <adeav97@gmail.com>
2023-11-06 20:28:46 -0800

    Added styling and add to cart to the product cards

--- client/src/components/Product.jsx
+++ client/src/components/Product.jsx
@@ -42,14 +42,15 @@ const Product = (product) => {
                             {productName}
                         </li>
                     </ul>
-                    <ul className="flex justify-between m-2 p-2">
+                    <ul className="flex justify-between m-1 p-1">
                         <li>
                             $ {salePrice.toFixed(2)}
                         </li>
-                        <button onClick={addToCart} type="button" className="text-black #D1D5DB focus:ring-4 focus:outline-none  font-medium rounded-lg text-sm  text-center inline-flex items-center ">
-                            <ShoppingBagIcon className=" w-4 cursor-pointer text-gray hover:scale-150 ease-in duration-300" />
-                            {/* <path d="M15 12a1 1 0 0 0 .962-.726l2-7A1 1 0 0 0 17 3H3.77L3.175.745A1 1 0 0 0 2.208 0H1a1 1 0 0 0 0 2h.438l.6 2.255v.019l2 7 .746 2.986A3 3 0 1 0 9 17a2.966 2.966 0 0 0-.184-1h2.368c-.118.32-.18.659-.184 1a3 3 0 1 0 3-3H6.78l-.5-2H15Z" /> */}
+                        
+                        <button onClick={addToCart} type="button" className="text-black #D1D5DB  focus:outline-none font-medium rounded-lg text-sm text-center flex">Add to Bag
+                            <ShoppingBagIcon className="ml-1 w-4 cursor-pointer text-gray hover:scale-150 ease-in duration-300" />
                         </button>
+                      
 
                     </ul>
                 </div>
